eb82fedf789dd3d182ff3f4c516508b28c9d2252
Keith Bennett <86028884+kbueso@users.noreply.github.com>
2021-12-21 11:38:37 -0600

    otros

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,12 @@
+export * from './licencias-transportistas.controller';
 export * from './licencias.controller';
 export * from './ping.controller';
+export * from './posiciones-rutas.controller';
 export * from './posiciones.controller';
 export * from './recorridos.controller';
 export * from './rutas.controller';
+export * from './tipovehiculo-vehiculos.controller';
 export * from './tipovehiculo.controller';
 export * from './transportistas.controller';
 export * from './vehiculos.controller';
-export * from './licencias-transportistas.controller';
-export * from './posiciones-rutas.controller';
-export * from './tipovehiculo-vehiculos.controller';
+
